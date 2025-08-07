858a7bede082167982df80be23f73b35f50eb5ed
dreidgon <38965160+dreidgon@users.noreply.github.com>
2025-08-07 12:50:04 +0100

    Added Plugin Info

---
+++ simple-testimonial-cards.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Simple Testimonial cards
+Description: Adds a testimonial custom post type and shortcode to display them.
+Version: 1.0
+Author: Dreidgon
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: simple-testimonials
+*/
