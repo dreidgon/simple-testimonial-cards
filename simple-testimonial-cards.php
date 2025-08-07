a7a1ed234b94ccb1171d25e2ad8fa2727359641d
dreidgon <38965160+dreidgon@users.noreply.github.com>
2025-08-07 13:00:55 +0100

    Added security check

--- simple-testimonial-cards.php
+++ simple-testimonial-cards.php
@@ -9,3 +9,19 @@ License: GPL v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: simple-testimonials
 */
+
+/**
+ * Security check:
+ * ABSPATH is a WP constant, if it's not defined, 
+ * it tipically means that the plugin file is being
+ * accessed outside of WP environment* 
+ */
+ 
+
+
+
+
+if ( ! defined('ABSPATH')){
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
